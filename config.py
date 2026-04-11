ca724263d5b851e87c76c0280087e7f688d07f66
Engineer Youssef Mahmoud <youssefaboali122@gmail.com>
2026-04-11 11:57:49 +0200

    Update config.py
    Remove unnecessary newline in Settings class.

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 class Settings:
 
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-
     MODEL_URI: str = os.getenv("MODEL_URI")
     ALLOWED_ORIGINS: list = os.getenv("ALLOWED_ORIGINS", "*").split(",")
 
